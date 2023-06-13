6202f719e507b092095ba69cf0c4c3d1b26fcca0
Tobias Wyss <towyss@gmail.com>
2023-06-13 10:56:24 +0200

    add examples to comments

--- docs/userTest.js
+++ docs/userTest.js
@@ -1,12 +1,13 @@
 import { from }      from "./Kolibri/contrib/wild_wyss/src/jinq/jinq.js";
 import { JsonMonad } from "./Kolibri/contrib/wild_wyss/src/json/jsonMonad.js";
 import * as _        from "./Kolibri/contrib/wild_wyss/src/iterator/iterator.js";
+const TODO = _ => undefined;
 
 // --------------------------------------------  INTRO  ----------------------------------------------------------------
 
 /**
  * Welcome & thanks that you will solve our user test, we appreciate it very much!
- * This user test is divided up in two parts:
+ * This user test is divided up into two parts:
  * 1. The power of lazy sequences
  * 2. The easy way to query different data structures
  *
@@ -24,17 +25,23 @@ import * as _        from "./Kolibri/contrib/wild_wyss/src/iterator/iterator.js"
 /**
  * In this project we built a set of functions to produce and transform lazy sequences.
  * A lazy sequence is a sequence of values, which never fully materialize itself in memory.
- * Therefore, lazy sequences can consist of infinite values.
+ * Therefore, lazy sequences can consist of an infinite amount of values.
  *
- * Such sequences can be created using a simple Constructor, taking three values:
+ * Such sequences can be created using a simple constructor, taking three values:
  * 1. An initial value, which is the first value to be returned.
  * 2. An until function, which decides on behalf of the value if further elements can be generated, and returns false if not
  * 3. An incrementation function, which defines how the next value is calculated based on the current value.
  *
  * In the following you will solve small exercises based on lazy sequences.
  *
+ */
+
+/**
+ *
+ * Look at this simple example of such a lazy sequence, which produces the values from 0 to 100!
+ *
  * _Note 1:_ The lazy sequence module is imported as {@link _}. This way you can easily access all operations on sequences using `_.`;
- *           For example to call the function map just use `const mapped = _.map(x => 2*x)([1,2,3,4,5]);`
+ *           For example to call the function map just use `const mapped = _.map(x => 2*x)([1,2,3,4,5]);`.
  *
  * _Note 2:_ Since sequences are lazy, there is no direct element access on a sequence.
  *           But you can easily deconstruct a sequence using the `...`-operator.
@@ -42,14 +49,16 @@ import * as _        from "./Kolibri/contrib/wild_wyss/src/iterator/iterator.js"
  *
  * _Note 3:_ Pay attention with infinite iterators, as they, when deconstructured try to eagerly evaluate each element of the sequence.
  *           This will go on forever and your browser will not respond anymore. Use {@link _.take} to only evaluate a certain amount of values.
+ *
+ * @type { IteratorMonadType<Number> }
+ *
  */
-
-// This is a simple example of such a lazy sequence, which produces the values from 0 to 100
 const seq1 = _.Iterator(0, i => i + 1, i => i < 100);
 
 
 /**
- * TODO: implement this function repeatF
+ * TODO 1: Now follows the first task: implement this function {@link repeatF} which returns an {@link IteratorMonadType }.
+ * Like in the example above - use the {@link _.Iterator}!
  *
  * repeatF takes a function `f` and a value `x` as arguments and creates an infinite
  * sequence starting with `x` and applying `f` to the last returned value in each iteration.
@@ -59,21 +68,28 @@ const seq1 = _.Iterator(0, i => i + 1, i => i < 100);
  * @param { _T_ } x            - the initial value
  *
  * @returns { IteratorMonadType<_T_> }
+ * @example
+ * const f = x => x + 1;
+ * const repeated = repeatF(f, 0);
+ *
+ * console.log(..._.take(4)(repeated));
+ * // => Logs '0, 1, 2, 3'
  */
 const repeatF = (f, x) => _.Iterator(x, f, _ => false);
 
-
 /**
+ * (You don't have to change this)
+ *
  * Halves the given value.
  * @param { Number } x
  * @returns { Number }
  */
 const halve = x => x / 2;
 
-/** TODO 2: implement this function halves
+/** TODO 2: implement this function halves, to create a simple sequence
  *
- * halves is an infinite sequence which halves the initial value on each iteration.
- * The sequence should halve the previous value in each iteration. Use the function {@link halve}.
+ * halves is an infinite sequence which halves the previous value in each iteration.
+ * Use the function {@link halve} defined above.
  *
  * @param   { Number } h0
  * @returns { IteratorMonadType<Number> }
@@ -86,15 +102,13 @@ const halve = x => x / 2;
 const halves = h0 => repeatF(halve, h0);
 
 /**
+ * (You don't have to change this)
  *
  * Now that you have a feeling about sequences you are ready to solve an exercise with a little more complexity:
  * Given is the following function {@link diff}:
  *
+ * {@link diff} finds the slope of a given function f at a given value x, with h approaching 0.
  *
- * {@link diff} finds the slope of a given function f at a given value x if h approaches 0.
- * So it is the basic formula to get the slope of a function f.
- *
- * @callback
  * @type {
  *            (f: (x: Number) => Number)
  *         => (x: Number)
@@ -105,6 +119,8 @@ const halves = h0 => repeatF(halve, h0);
 const diff = f => x => h => (f(x + h) - f(x)) / h;
 
 /**
+ * (You don't have to change this)
+ *
  * An example function - we will calculate its derivative later.
  * @param  { Number } x
  * @return { Number }
@@ -112,13 +128,15 @@ const diff = f => x => h => (f(x + h) - f(x)) / h;
 const f = x => x * x;
 
 /**
- * TODO 4:
- * The problem with this function is, to determine an h that is close enough to zero.
+ * TODO 3: finding a good h
+ * The difficulty when using this function is, to determine an h that is close enough to zero.
  * Fortunately, we can use our function {@link halves} to generate smaller and smaller values from a given starting point h0!
  *
  *
- * Implement the following function {@link differentiate}, which takes a starting value h0, a function f and a value x.
- * This function will then return a Sequence of {@link Number Numbers} approaching closer and closer the real slope of f at the value x!
+ * Implement the following function {@link differentiate}, which takes a starting value `h0`, a function `f` and a value `x`.
+ * This function will then return a Sequence of {@link Number Numbers} approaching closer and closer to the real
+ * slope of `f` at the value `x`!
+ *
  * _Note:_ use {@link _.map} to map the function {@link diff} over the halves!
  *
  * _Note 2:_ Use the function {@link f} defined above to test your implementation.
@@ -130,11 +148,17 @@ const f = x => x * x;
  *       => (x: Number)
  *       => IteratorMonadType<Number>
  * }
+ *
+ * @example
+ * const diffs = differentiate(0.5)(f)(1);
+ * console.log(..._.take(5)(diffs));
+ *
+ * // => Logs '2.5, 2.25, 2.125, 2.0625, 2.03125'
  */
 const differentiate = h0 => f => x => _.map(diff(f)(x))(halves(h0));
 
 /**
- * TODO 5:
+ * TODO 4: finding more accurate slopes
  * We can now differentiate which is great.
  * But it would even be better if we wouldn't get an infinite sequence of values, but only one value which is accurate enough.
  *
@@ -149,9 +173,13 @@ const differentiate = h0 => f => x => _.map(diff(f)(x))(halves(h0));
  *   => Number
  * }
  *
+ * @example
+ * const smallDifference = within(0.1)(halves(10));
+ * console.log(smallDifference);
+ * // => Logs '0.078125'
  */
-const within = eps => list => {
-  const [a, rest] = _.uncons(list);
+const within = eps => sequence => {
+  const [a, rest] = _.uncons(sequence);
   const [b]       = _.uncons(rest);
   const diff      = Math.abs(a - b);
 
@@ -159,26 +187,23 @@ const within = eps => list => {
   return within(eps)(rest);
 };
 
-
 /**
- * TODO 6:
+ * TODO 5: Let's combine the created functions!
  *
- * Let's combine the created functions!
+ * Use your function {@link differentiate} to approximate the slope of the function {@link f} at the point `1.0`.
+ * Use `0.5` as starting point for `h0`.
  *
- * Use your function {@link differentiate} to approximate the derivative of the function {@link f} at the point 1.0.
- * Use 0.5 as starting point for h0.
- *
- * Then pass the sequence to your function {@link within} to get the derivative with an approximation of 0.0001.
+ * Then pass the sequence to your function {@link within} to get the slope with an approximation of `0.0001`.
  *
  */
-const derivativeSequence = differentiate(0.5)(f)(1.0);
-const derivativeOfFAtX = within(0.0001)(derivativeSequence);
+const seqOfSlopes = differentiate(0.5)(f)(1.0);
+const slopeOfFAtX = within(0.0001)(seqOfSlopes);
 
-console.log(derivativeOfFAtX);
+console.log(slopeOfFAtX);
 
 /**
  * Conclusion:
- * With lazy sequences it is very easy to calculate a derivative with arbitrary accuracy.
+ * With lazy sequences it is very easy to calculate slopes of functions with arbitrary accuracy.
  * Additionally, it allows us to split the code into many small pieces. These are separately testable, changeable or extendable.
  *
  * Imagine you don't want to halve the original h0 but quarter it with each step. To do this, simply
